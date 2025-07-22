7473ef23c545561d86ae640efc71754792518774
Yefry Gabriel García Caxaj <caxajgabriel2@gmail.com>
2025-07-21 23:40:51 -0600

    menu principal

---
+++ principal.py
@@ -0,0 +1,11 @@
+while True:
+    print("\n*** MENU PRINCIPAL ***")
+    print("1 - Factorial")
+    print("2 - Suma de los primeros n números")
+    print("3 - Calcular n-ésimo numro de Fibonacci")
+    print("4 - Contar letra en una palabra")
+    print("5 - Invertir una cadena de texto")
+    print("6 - Calcular potencia")
+    print("0 - Salir")
+
+    opcion = input("Seleccione una opción: ")

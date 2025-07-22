41426e6d1f3c619d8ea80d922c2f7ecc304ac643
Yefry Gabriel García Caxaj <caxajgabriel2@gmail.com>
2025-07-21 23:47:44 -0600

    factorial

--- principal.py
+++ principal.py
@@ -1,3 +1,9 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
 while True:
     print("\n*** MENU PRINCIPAL ***")
     print("1 - Factorial")
@@ -9,3 +15,19 @@ while True:
     print("0 - Salir")
 
     opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        num = 0
+
+        while True:
+            try:
+                num = int(input("Ingrese un número entero positivo para calcular su factorial: "))
+                if num >= 0:
+                    break
+            except ValueError:
+                print("Por favor, ingrese un número válido.")
+
+        factorial = factorial(num)
+
+    if opcion == "2":
+        num = 0
